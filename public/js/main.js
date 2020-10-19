63e8e2a28dcb244691b86b51d80cded57790a533
Mayank Rawat <52965332+m-rawat@users.noreply.github.com>
2020-10-19 18:20:50 +0530

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,49 @@
+const cityName=document.getElementById('cityName');
+const submitBtn=document.getElementById('submitBtn');
+const city_name=document.getElementById('city_name');
+const temp_real_val=document.getElementById('temp_real_val');
+const temp_status=document.getElementById('temp_status');
+const dataHide=document.querySelector('.middle_layer');
+
+const getInfo=async(event) =>{
+    event.preventDefault();
+    let cityVal=cityName.value;
+    if(cityVal===" "){
+        city_name.innerText=`Please Write City Name`;
+        dataHide.classList.add('data_hide');
+    }
+    else{
+        try{
+            let url=`http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=a07c5c76cb0ad7e4ee94142e598f5cd2`;
+            const response= await fetch(url);
+            const data= await response.json();
+            const arrData=[data];
+
+            city_name.innerText=`${arrData[0].name},${arrData[0].sys.country}`;
+            temp_real_val.innerText=arrData[0].main.temp;
+
+            const tempMood=arrData[0].weather[0].main;
+
+            if(tempMood=="Clear"){
+                temp_status.innerHTML="<i class='fas fa-sun' style='color:#eccc68;'></i>";
+            }
+            else if(tempMood=="Clouds"){
+                temp_status.innerHTML="<i class='fas fa-cloud' style='color:#f1f2f6;'></i>";
+            }
+            else if(tempMood=="Rain"){
+                temp_status.innerHTML="<i class='fas fa-cloud-rain' style='color:#a4b0be;'></i>";
+            }
+            else{
+                temp_status.innerHTML="<i class='fas fa-cloud' style='color:#f1f2f6;'></i>";
+            }
+            dataHide.classList.remove('data_hide');
+        }catch{
+            city_name.innerText=`Please Write City Name`;
+            dataHide.classList.add('data_hide');
+        }
+
+        
+    }
+}
+submitBtn.addEventListener('click', getInfo);
+
